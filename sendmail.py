d0170bf815e27b7aeb46c0892bf5aefdd21409b0
whitecloud <jojoama@yahoo.com>
2016-07-29 11:22:26 +0100

    updated

--- sendmail.py
+++ sendmail.py
@@ -2,7 +2,7 @@ import smtplib
  
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("chijioke.amaefuna@gmail.com", "Spanner_2009")
+server.login("chijioke.amaefuna@gmail.com", "cat_2009")
  
 msg = "testing"
 server.sendmail("chijioke.amaefuna@gmail.com", "ddebisiolaniyan@gmail.com", msg)
